15b262718b5c10446915424e67a17f30eab2df14
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-08-12 11:41:41 +0200

    [Created] static chessboard

--- checkers_game.py
+++ checkers_game.py
@@ -1,1 +1,16 @@
 # Entrypoint file
+
+# Static Chessboard
+initial_chessboard = [
+    ["B", 0, "B", 0, "B", 0, "B", 0],   # line 0 - first line from the top
+    [0, "B", 0, "B", 0, "B", 0, "B"],   # line 1
+    ["B", 0, "B", 0, "B", 0, "B", 0],   # line 2
+    [0, "F", 0, "F", 0, "F", 0, "F"],   # line 3
+    ["F", 0, "F", 0, "F", 0, "F", 0],   # line 4
+    [0, "W", 0, "W", 0, "W", 0, "W"],   # line 5
+    ["W", 0, "W", 0, "W", 0, "W", 0],   # line 6
+    [0, "W", 0, "W", 0, "W", 0, "W"],   # line 7
+]
+
+
+
