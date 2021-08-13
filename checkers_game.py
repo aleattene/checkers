e27d8fb79a0b28d78276122d7dbc8e3b43e556d7
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-08-13 12:23:32 +0200

    [Removed] some comments

--- checkers_game.py
+++ checkers_game.py
@@ -14,8 +14,8 @@ def main():
     # Make move
     move = Move(chessboard)
 
-    print(move.move_from_row)
-    print(move.move_from_col)
+    # print(move.move_from_row)
+    # print(move.move_from_col)
     # print(initial_chessboard[move_from_row][move_from_col])
 
     move_from = False
@@ -24,15 +24,15 @@ def main():
     if chessboard.disposition[move.move_from_row][move.move_from_col] == "W":
         move_from = True
 
-    print(move.move_to_row)
-    print(move.move_to_col)
+    # print(move.move_to_row)
+    # print(move.move_to_col)
     # print(initial_chessboard[move_to_row][move_to_col])
 
     if chessboard.disposition[move.move_to_row][move.move_to_col] == "F":
         move_to = True
 
-    print(move_from)
-    print(move_to)
+    # print(move_from)
+    # print(move_to)
 
     if move_from and move_to:
         print("Mossa Consentita")
